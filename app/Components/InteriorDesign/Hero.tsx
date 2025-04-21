6394e5ccd71c0b357ddd0a49a8163a75b072f750
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2025-04-22 00:45:04 +0200

    addd error page

--- app/Components/InteriorDesign/Hero.tsx
+++ app/Components/InteriorDesign/Hero.tsx
@@ -67,16 +67,6 @@ const Hero = () => {
         ))}
       </Swiper>
     </div>
-
-    // <section className="bg-[url('/common/designHero.jpg')] h-[calc(100dvh-68px)] bg-cover bg-bottom">
-
-    //   <Overlay className="flex justify-center items-center font-titles text-white">
-    //     <h1 className="text-4xl 2xl:text-[100px] uppercase text-center font-bold tracking-tighter font-titles w-[80%] m-auto">
-    //       Tuka moze da stoi edna slika ili da se menuvaat
-    //     </h1>
-    //     <></>
-    //   </Overlay>
-    // </section>
   );
 };
 
