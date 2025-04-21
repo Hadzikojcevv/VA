1aea6ff1ff255de74dd093dbb4e45e2ae09b1e4a
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2025-04-22 00:46:17 +0200

    spell corrected

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
   return (
     <section className='h-[100dvh] flex flex-col items-center justify-center text-center'>
         <Image src={'/common/VA_logo.png'} alt='Logo' width={300} height={300} className='w-[150px] mb-6'/>
-        <h1 className='text-4xl lg:text-5xl font-black tracking-tighter'>Ooops, Nothnin&#39;s Here!</h1>
+        <h1 className='text-4xl lg:text-5xl font-black tracking-tighter'>Ooops, Nothning&#39;s Here!</h1>
         <p className='text-lg lg:text-xl tracking-tighter py-6 '>You must have taken a wrong turn. <br /> Get back on the right tracks now!</p>
         <Link href={'/'} className='px-10 py-4 bg-main-dark text-white font-semibold tracking-tighter uppercase'>Homepage</Link>
     </section>
